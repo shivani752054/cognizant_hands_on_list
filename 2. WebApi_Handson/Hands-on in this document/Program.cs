d218731a27472a2282b74ba909a42c57b63c1bb9
shivani_752054 <shvnpanigrahi04@gmail.com>
2026-07-03 20:52:08 +0530

    Add files via upload

---
+++ 2. WebApi_Handson/Hands-on in this document/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// ---- Services ----
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Swagger Demo",
+        Version = "v1",
+        Description = "TBD",
+        Contact = new OpenApiContact
+        {
+            Name = "John Doe",
+            Email = "john@xyzmail.com",
+            Url = new Uri("https://www.example.com")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "License Terms",
+            Url = new Uri("https://www.example.com")
+        }
+    });
+});
+
+var app = builder.Build();
+
+// ---- Pipeline ----
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    // specifying the Swagger JSON endpoint.
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger Demo");
+});
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
